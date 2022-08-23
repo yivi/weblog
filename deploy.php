769a1fe8e469462da6d8c7550be8d48727f77fd3
Ivan Yivoff <ivan@yivoff.com>
2022-08-23 14:20:16 +0200

    deploy recipe

---
+++ deploy.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/common.php';
+
+// Config
+
+set('repository', '');
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+task('hugo:rebuild', function () {
+    run("cd {{release_path}} && hugo --minify");
+});
+
+// Hosts
+
+host('ludoteca')
+    ->set('remote_user', 'deployer')
+    ->set('deploy_path', '~/weblog-hugo');
+
+// Hooks
+
+task('deploy', [
+    'deploy:prepare',
+    'hugo:rebuild',
+    'deploy:publish',
+])->desc('Deploy your project');
